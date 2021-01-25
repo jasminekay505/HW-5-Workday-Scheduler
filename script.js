0143df51e3550c77ccd4d0efbf954a9f85b4c685
Jasmine Kay <jasminekay505@gmail.com>
2021-01-25 15:14:50 -0800

    Added save icon to button and bootstrap classes

--- script.js
+++ script.js
@@ -4,14 +4,16 @@ $(document).ready(function() {
     //Set up HTML with time intervals and save buttons
     var hours = ["9", "10", "11", "12", "1", "2", "3", "4", "5"];
     var newRow = $("<div>").addClass("row");
-    var newCol = $("<div>").addClass("col-md-1");
-    var newEntry = $("<textarea>").addClass("col-md-10");
+    var newHour = $("<div>").addClass("col-md-1");
+    var newEntry = $("<textarea>").addClass("col-md-10 form-control");
     var newButton = $("<button>").addClass("col-md-1 btn btn-primary");
-    
+    var saveIcon = $('<i class="far fa-save fa-2x"></i>');
+
     $(".container").append(newRow);
-    newRow.append(newCol);
+    newRow.append(newHour);
     newRow.append(newEntry);
     newRow.append(newButton);
+    newButton.append(saveIcon);
 
 
 
