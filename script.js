b51b10d6c7933cd2568e391f89f8107d3b025d68
Jasmine Kay <jasminekay505@gmail.com>
2021-01-25 15:25:03 -0800

    Added for loop to create all time slots and save buttons

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
 
     //Set up HTML with time intervals and save buttons
-    var hours = ["9", "10", "11", "12", "1", "2", "3", "4", "5"];
-    var newRow = $("<div>").addClass("row");
-    var newHour = $("<div>").addClass("col-md-1");
-    var newEntry = $("<textarea>").addClass("col-md-10 form-control");
-    var newButton = $("<button>").addClass("col-md-1 btn btn-primary");
-    var saveIcon = $('<i class="far fa-save fa-2x"></i>');
+    var hours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+    for (var i = 0; i < hours.length; i++) {
+        var newRow = $("<div>").addClass("row");
+        var newHour = $("<div>").addClass("col-md-1 hour");
+        var newEntry = $("<textarea>").addClass("col-md-10 form-control");
+        var newButton = $("<button>").addClass("col-md-1 saveBtn");
+        var saveIcon = $('<i class="far fa-save fa-2x"></i>');
 
-    $(".container").append(newRow);
-    newRow.append(newHour);
-    newRow.append(newEntry);
-    newRow.append(newButton);
-    newButton.append(saveIcon);
+        $(".container").append(newRow);
+        newRow.append(newHour);
+        newRow.append(newEntry);
+        newRow.append(newButton);
+        newButton.append(saveIcon);
+        newHour.text(hours[i])
+
+    }
 
 
 
