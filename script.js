0e5eb3c276881ff84631bcf5415842f407eba333
Jasmine Kay <jasminekay505@gmail.com>
2021-01-25 15:00:29 -0800

    Setting up JS to create time intervals and save buttons in a loop for each time slot

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
 
 
+    //Set up HTML with time intervals and save buttons
+    var hours = ["9", "10", "11", "12", "1", "2", "3", "4", "5"];
+    var newRow = $("<div>").addClass("row");
+    var newCol = $("<div>").addClass("col-md-1");
+    var newEntry = $("<textarea>").addClass("col-md-10");
+    var newButton = $("<button>").addClass("col-md-1 btn btn-primary");
+    
+    $(".container").append(newRow);
+    newRow.append(newCol);
+    newRow.append(newEntry);
+    newRow.append(newButton);
+
+
+
 });
